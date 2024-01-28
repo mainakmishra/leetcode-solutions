4f4ea118046d0b5762eab894698b46c0290e4416
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-01-29 02:27:05 +0530

    Time: 8 ms (12.86%) | Memory: 9.1 MB (9.48%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -7,11 +7,12 @@ public:
             mp[s[i]]=i;
         }
         vector <int> ans;
-        int start=0,end=0;
+        int start=0,end=mp[s[0]];
         for(i=0;i<n;i++){
-            end=max(end,mp[s[i]]);
+            if(i<end)end=max(end,mp[s[i]]);
             if(i==end){
                 ans.push_back(end-start+1);
+                if(i<n-1)end=mp[s[i+1]];
                 start=i+1;
             }
         }
