93b1f1dc2520244e3c49ddcea34fa1313ad22ae3
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-02-03 17:01:09 +0530

    Time: 78 ms (92.59%) | Memory: 43.4 MB (58.93%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,5 +1,4 @@
 class Solution {
-private:
 
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
@@ -7,22 +6,19 @@ public:
         for(auto &i:times){
             adj[i[0]].push_back({i[1],i[2]});
         }
-        vector <int> vis(n+1,0);
         vector <int> dist(n+1,1e9);
         dist[k]=0;
         dist[0]=0;
         queue <pair<int,int>> q;
         q.push({k,0});
-        vis[k]=1;
         map <int,int> mp;
         while(!q.empty()){
             int node=q.front().first;
             int d=q.front().second;
             q.pop();
-            
             for(auto &i:adj[node]){
                 int x=i.first;
-                
+                //if(dist[x]<d+i.second)continue;
                 if(dist[x]>d+i.second){
                     dist[x]=d+i.second;
                     q.push({x,dist[x]});
