fe3f78523ac14e0c03018bf2ad546edb5c7a2a19
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-02-03 12:56:16 +0530

    Time: 12 ms (93.10%) | Memory: 16.8 MB (27.45%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    private:
+    void dfs(int vertex, vector<vector<int>> &adj, vector<int> visited){
+
+    }
+public:
+    bool canFinish(int n, vector<vector<int>>& course) {
+        vector<vector<int>> adj(n);
+        for(auto &i:course){
+            adj[i[1]].push_back(i[0]);
+        }
+        vector <int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue <int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector <int> ans;
+        while(!q.empty()){
+            int node=q.front();
+            ans.push_back(node);
+            q.pop();
+            for(auto &it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()==n)return true;
+        else return false;
+
+    }
+};
