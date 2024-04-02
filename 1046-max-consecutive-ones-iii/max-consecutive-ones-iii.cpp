49587e81693ec902ad6b5c864a13f7b54575c1bc
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-03 00:59:29 +0530

    Time: 21 ms (99.51%) | Memory: 57.8 MB (49.49%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -8,11 +8,9 @@ public:
         int n=a.size();
         int left=0;
         int ans=0,current=0,count=0;
-        for(auto &i:a)if(i)++count;
-        if(k==0 and count==0)return 0;
         for(int i=0;i<n;i++){
             if(a[i]==0)current++;
-            while(current>k and left<n-1){
+            while(current>k and left<n){
                 if(a[left]==0)--current;
                 ++left;
             }
