f7b16893bc2fb283cc535ae6d47316670203d869
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-03 00:56:56 +0530

    Time: 32 ms (91.38%) | Memory: 58 MB (13.00%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-
+    Solution(){
+        ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
     int longestOnes(vector<int>& a, int k) {
         int n=a.size();
         int left=0;
