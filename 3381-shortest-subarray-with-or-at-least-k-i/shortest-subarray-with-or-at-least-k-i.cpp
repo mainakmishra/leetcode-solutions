ff6dc80f8a9f7197036bbecb76f747198db753b8
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-03 01:34:30 +0530

    Time: 7 ms (48.63%) | Memory: 25.8 MB (86.67%) - LeetSync

---
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& a, int k) {
+        int right=0;
+        int n=a.size();
+        int ans=1e9;
+        for(int i=0;i<n;i++){
+            int x=0;
+            for(int j=i;j<n;j++){
+                x=x|a[j];
+                if(x>=k){
+                    ans=min(ans,j-i+1);
+                    break;
+                }
+            }
+        }
+        if(ans==1e9)return -1;
+        return ans;
+    }
+};
