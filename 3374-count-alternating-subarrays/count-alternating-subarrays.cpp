690d81cf401f0cd9afe13241face34807e459dcd
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-02 11:28:22 +0530

    Time: 66 ms (98.33%) | Memory: 116.5 MB (10.34%) - LeetSync

--- 3374-count-alternating-subarrays/count-alternating-subarrays.cpp
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.cpp
@@ -7,16 +7,8 @@ public:
     long long countAlternatingSubarrays(vector<int>& a) {
         long long right=0,ans=0;
         long long n=a.size();
-        long long flag=1;
-        for(long long i=1;i<n;i++){
-            if(a[i]==a[i-1]){
-                flag=0;
-                break;
-            }
-        }
-        if(flag==1)return 1LL*n*(n+1)/2;
         for(long long i=0;i<n;i++){
-            right=i;
+            right=max(i,right);
             while(right<n-1 and a[right]!=a[right+1])right++;
             ans+=right-i+1;
         }
