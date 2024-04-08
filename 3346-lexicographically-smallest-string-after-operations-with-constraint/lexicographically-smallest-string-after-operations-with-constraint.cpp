92d97b6dbe395acd68ce18793f1e0b241eaa2d19
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-09 00:22:42 +0530

    Time: 4 ms (60.00%) | Memory: 8.6 MB (40.00%) - LeetSync

---
+++ 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dist(char a,char b){
+        if(a>b)swap(a,b);
+        int ans=min(b-a,a-'a'+'z'-b+1);
+        return ans;
+    }
+    string getSmallestString(string s, int k) {
+        if(k==0)return s;
+        vector <char> alpha;
+        for(char i='a';i<='z';i++)alpha.push_back(i);
+        string t="";
+        for(int i=0;i<s.length();i++){
+            int j=(s[i]-97+1)%26;
+            int maxi=0;
+            if(k==0 or s[i]=='a'){
+                t+=s[i];
+                continue;
+            }
+            char flag='z';
+            while(alpha[j]!=s[i]){
+                int d=dist(s[i],alpha[j]);
+                if(d<=k){
+                    if(alpha[j]<=flag){
+                        maxi=d;
+                        flag=alpha[j];
+                    }
+                }
+                j=(j+1)%26;
+            }
+            k-=maxi;
+            t+=flag;
+        }
+        return t;
+    }
+};
