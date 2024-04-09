04b72dac94e7dc23f507120d83bdfae6ae4e4b47
mainakmishra <60008514+mainakmishra@users.noreply.github.com>
2024-04-10 00:19:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (67.56%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans=0;
+        while(1){
+            for(int i=0;i<tickets.size();i++){
+                if(tickets[i]>0){
+                    tickets[i]--;
+                    ans++;
+                }
+                if(tickets[k]==0)return ans;
+            }
+            
+        }
+    }
+};
