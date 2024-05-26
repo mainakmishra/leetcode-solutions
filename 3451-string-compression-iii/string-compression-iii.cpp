bd94084bb6a6b9f2fcafeb132b07801442f9745d
Mainak Mishra <60008514+mainakmishra@users.noreply.github.com>
2024-05-26 09:44:51 +0530

    Time: 85 ms (40.00%) | Memory: 28.6 MB (20.00%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -6,8 +6,7 @@ public:
         int count=1;
         for(int i=0;i<n;i++){
             if(i==n-1 or word[i]!=word[i+1] or count>=9){
-                ans+=to_string(count);
-                ans+=word[i];
+                ans+=to_string(count)+word[i];
                 count=1;
             }
             else count++;
