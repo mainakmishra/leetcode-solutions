f2f741d0426bc7b4610895f8fd8e39c0e9953b11
Mainak Mishra <60008514+mainakmishra@users.noreply.github.com>
2024-09-19 01:29:42 +0530

    Time: 11 ms (34.83%) | Memory: 26.3 MB (63.71%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        set <int> st;
+        vector <int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(st.find(nums[i])!=st.end())ans.push_back(nums[i]);
+            else st.insert(nums[i]);
+        }
+        return ans;
+    }
+};
