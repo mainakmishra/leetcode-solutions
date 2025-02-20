10e4c5c79a3959acc10656f672cdfa8c852437a2
Mainak Mishra <60008514+mainakmishra@users.noreply.github.com>
2025-02-20 12:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (19.29%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,33 +1,24 @@
-
-#include <vector>
-#include <string>
-#include <unordered_set>
-
-using namespace std;
-
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums.size();
-        unordered_set<string> numSet(nums.begin(), nums.end());
-        return generateBinaryStrings("", numSet, n);
+    int N;
+    string ans="";
+
+    void f(string s, set<string> &st) {
+        if (!ans.empty()) return; 
+        if (s.length() == N) {
+            if (st.find(s) == st.end()) {
+                ans = s;
+            }
+            return;
+        }
+        f(s + '0', st);
+        f(s + '1', st);
     }
 
-private:
-    string generateBinaryStrings(string current, unordered_set<string>& numSet, int n) {
-        if (current.length() == n) {
-            if (numSet.find(current) == numSet.end()) {
-                return current;
-            }
-            return "";
-        }
-
-        for (char c : {'0', '1'}) {
-            string binaryString = generateBinaryStrings(current + c, numSet, n);
-            if (!binaryString.empty()) {
-                return binaryString;
-            }
-        }
-        return "";
+    string findDifferentBinaryString(vector<string>& nums) {
+        N = nums[0].length();
+        set<string> st(nums.begin(), nums.end());
+        f("", st);
+        return ans;
     }
 };
